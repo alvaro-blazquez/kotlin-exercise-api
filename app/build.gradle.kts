07890059a06d53dec693fe4511f12b5af3d6065f
alvaro <alvablaz@gmail.com>
2024-11-10 17:14:29 +0100

    Add functional tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,19 @@ repositories {
     mavenCentral()
 }
 
+sourceSets {
+    create("functionalTest") {
+        compileClasspath += sourceSets.main.get().output
+        runtimeClasspath += sourceSets.main.get().output
+    }
+}
+
+val functionalTestImplementation: Configuration by configurations.getting {
+}
+
 dependencies {
     testImplementation(libs.kotest)
+    functionalTestImplementation(libs.kotest)
 }
 
 java {
@@ -23,4 +34,13 @@ application {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+
+val functionalTest = task<Test>("functionalTest") {
+    description = "Runs functional tests."
+    group = "verification"
+
+    testClassesDirs = sourceSets["functionalTest"].output.classesDirs
+    classpath = sourceSets["functionalTest"].runtimeClasspath
 }
