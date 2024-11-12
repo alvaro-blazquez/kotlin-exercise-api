4e34de640e3b091cdc14d8231699e8d63ae188e3
alvaro <alvablaz@gmail.com>
2024-11-12 22:02:34 +0100

    Create mutable list to store orders in the service

--- app/src/main/kotlin/org/example/domain/OrderService.kt
+++ app/src/main/kotlin/org/example/domain/OrderService.kt
@@ -1,6 +1,7 @@
 package org.example.domain
 
-class OrderService(val orders: List<Order>) {
+class OrderService(orders: List<Order>) {
+    val orders: MutableList<Order> = orders.toMutableList()
 
     fun getOrderById(orderId: Int): Order? = orders.find { it.orderId == orderId }
 }
